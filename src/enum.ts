5ba3326fac8da85a6e4b86b124714bd9f8d18459
dilawfird <tiger_trading@outlook.fr>
2019-11-09 12:13:28 +0100

    Adding recaptchaV3 (#11)

--- src/enum.ts
+++ src/enum.ts
@@ -1,6 +1,7 @@
 export enum TaskTypes {
   RECAPTCHA_PROXYLESS = "NoCaptchaTaskProxyless",
   RECAPTCHA_PROXY = "NoCaptchaTask",
+  RECAPTCHA_V3_PROXYLESS = "RecaptchaV3TaskProxyless",
   IMAGE_TO_TEXT = "ImageToTextTask",
   FUN_CAPTCHA = "FunCaptchaTask",
 }
